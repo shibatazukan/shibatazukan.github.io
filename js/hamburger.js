03b938460d6a31282da39306486fc5e6c32e77de
member <ijirotamito@gmail.com>
2025-10-22 20:25:34 +0900

    Delete js/hamburger.js

--- js/hamburger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById('menuToggle');
-  const sideMenu = document.getElementById('sideMenu');
-
-  if (!menuToggle || !sideMenu) return;
-  menuToggle.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
-  });
-});
