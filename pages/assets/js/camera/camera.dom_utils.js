ffb6e238a90c4f4f75b956d59c2033812a695a4f
member <ijirotamito@gmail.com>
2026-02-05 19:59:45 +0900

    camera.dom_utils.js を更新

--- pages/assets/js/camera/camera.dom_utils.js
+++ pages/assets/js/camera/camera.dom_utils.js
@@ -19,11 +19,10 @@ const startButton         = document.getElementById('startButton');
 
 // 現在のモードを取得
 function getCurrentMode() {
-  /*
   const selected = document.querySelector('input[name="selectionMode"]:checked');
   return selected ? selected.value : 'full';
-  */
-  return 'rectangle';
+  
+ // return 'rectangle';
 }
 
 // 緯度経度から住所を取得（localStorageにキャッシュ）
